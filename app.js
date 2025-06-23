6cda79e9b93e48894e027763ba2e5198472857c0
Amanda Bueno <amandacocal12@gmail.com>
2025-06-23 11:07:43 -0300

    Update app and grafo code

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ const grafofilho = new Grafopai();
 grafofilho.leituraArqJSON('./utilitario/capitais.json');
 
 // adicionar endpoint para retornar lista de capitais
+app.get('/capitais', (req, res) => {
+    const listadecidades = Array.from(grafofilho.vert.keys());
+    res.json(listadecidades);
+})
 
 // adicionar endpoint para cálculo da rota
+app.post('/calculo-rota', (req, res) => {
+    const {origem, destino, precogasolina, autonomia} = req.body;
+
+    const result = dijkstra(grafofilho, origem, destino, precogasolina, autonomia);
+    res.json(result);
+})
 
 // adicionar rota para iniciar servidor
--- utilitario/grafo.js
+++ utilitario/grafo.js
@@ -6,18 +6,34 @@ class Grafopai {
     }
 
     // função para adicionar vértice
+    adicionaVert(nome, custopedagio) {
+        this.vert.set(nome, { custopedagio, vizinhos: new Map() });
+    }
 
     // função para adicionar aresta
+    adicionaAresta(origemviagem, destinoviagem, distancia) {
+        this.vert.get(origemviagem).vizinhos.set(destinoviagem, distancia);
+        this.vert.get(destinoviagem).vizinhos.set(origemviagem, distancia);
+    }
 
     leituraArqJSON(rota) {
-        const dados = JSON.parse(fs.readFileSync(caminho, 'utf8'));
+        const dados = JSON.parse(fs.readFileSync(rota, 'utf8'));
 
         for (const dadoselecionado of dados) {
             for (const cidade in dadoselecionado) {
-                const { ctapedagio, vizinhos } = dadoselecionado[cidade];
+                const { ctapedagio, vizinhanca } = dadoselecionado[cidade];
+                this.adicionaVert(cidade, ctapedagio);
 
                 // adicionar condições de inclusão de arestas e vértices
+                for (const vizinhoselecionado in vizinhanca) {
+                    this.adicionaVert(vizinhoselecionado, 0);
+                    this.adicionaAresta(cidade, vizinhoselecionado, vizinhanca[vizinhoselecionado]);
+                }
             }
         }
     }
+
+    // adicionar função de exibição do pedágio
 }
+
+module.exports = Grafopai;
