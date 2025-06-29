8d62f50168bbf3119aa295ffcccca7161762b1ba
Amanda Bueno <amandacocal12@gmail.com>
2025-06-29 00:51:02 -0300

    Create a part of frontend, with files html and js

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,8 @@
-// scripts.js
+document.getElementById('formulario-rota').addEventListener('calc', async (e) => {
+    e.preventDefault();
+    const origem = document.getElementById('origem').value;
+    const destino = document.getElementById('destino').value;
+    const precoComb = parseFloat(document.getElementById('precoComb').value);
+    const autonomia = parseFloat(document.getElementById('autonomia').value);
+
+});
